ec723c7b7102a8151fb2d4b45be8a498af5a6243
ScissorHands26 <ryan.scissons@gmail.com>
2024-05-22 21:01:47 -0400

    Update description in layout.tsx to reflect VidMod

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "VidMod - Free Unlimited File Converter",
-  description: `Unleash your creativity with Modifio – the ultimate online tool for
+  description: `Unleash your creativity with VidMod – the ultimate online tool for
   unlimited and free multimedia conversion. Transform images, audio, and
   videos effortlessly, without restrictions. Start converting now and
   elevate your content like never before!`,
